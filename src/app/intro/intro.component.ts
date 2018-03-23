141bf0e4bb6353a41467e8b8a850d048c871b25b
Joseph Turner <iamjoeturner@gmail.com>
2018-03-23 11:06:58 -0600

    adds jquery and svg library and a function to make hexagons, also updates versions

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent implements OnInit {
+
+  time_from_beginning = moment([2010, 5, 21]).fromNow(true);
 
   constructor() { }
 
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+
+  time_from_beginning = moment([2010, 5, 21]).fromNow(true);
 
   constructor() { }
 
