bcb854b6f0cbf0c2a01eeb4c009dd7ebd1f23d0f
Joseph Turner <iamjoeturner@gmail.com>
2018-04-12 12:21:11 -0600

    fixes interests spec

--- src/app/interests/interests.component.spec.ts
+++ src/app/interests/interests.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InterestsComponent } from './personal.component';
+import { InterestsComponent } from './interests.component';
 
 describe('InterestsComponent', () => {
   let component: InterestsComponent;
