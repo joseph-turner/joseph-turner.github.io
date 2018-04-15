5bf18e8daf67d388b0e3396b15afbf383bcfc710
Joseph Turner <iamjoeturner@gmail.com>
2018-04-15 13:41:01 -0600

    fixes firebase environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: true,
-  apiKey: 'AIzaSyCg4zzTZF-OaxBCfbvXr24E1M9befoaJ7w',
-  authDomain: 'joseph-turner-github-page.firebaseapp.com',
-  databaseURL: 'https://joseph-turner-github-page.firebaseio.com',
-  projectId: 'joseph-turner-github-page',
-  storageBucket: 'joseph-turner-github-page.appspot.com',
-  messagingSenderId: '815131106099'
+  firebase: {
+    apiKey: 'AIzaSyCg4zzTZF-OaxBCfbvXr24E1M9befoaJ7w',
+    authDomain: 'joseph-turner-github-page.firebaseapp.com',
+    databaseURL: 'https://joseph-turner-github-page.firebaseio.com',
+    projectId: 'joseph-turner-github-page',
+    storageBucket: 'joseph-turner-github-page.appspot.com',
+    messagingSenderId: '815131106099'
+  }
 };
